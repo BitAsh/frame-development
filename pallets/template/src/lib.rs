31574e72e47ed4aa682526498ae4fcb9084ae16e
Nicola A <x2077@pm.me>
2020-10-27 23:23:42 +1300

    Add accumulation.

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -4,6 +4,7 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
+use codec::{Encode, Decode};
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
 use frame_system::ensure_signed;
 
@@ -13,22 +14,25 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+/// The accumulation item.
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+pub struct Accumulation {
+	/// Current count of the accumulation.
+	pub current_count: u128,
+	/// Increment for each dispatch call.
+	pub increment_per_call: u128,
+}
+
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
 	/// Because this pallet emits events, it depends on the runtime's definition of an event.
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
-// The pallet's runtime storage items.
-// https://substrate.dev/docs/en/knowledgebase/runtime/storage
+
 decl_storage! {
-	// A unique name is used to ensure that the pallet's storage items are isolated.
-	// This name may be updated, but each pallet in the runtime must use a unique name.
-	// ---------------------------------vvvvvvvvvvvvvv
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Learn more about declaring storage items:
-		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
+		pub Acc get(fn acc): Accumulation;
 	}
 }
 
@@ -36,19 +40,14 @@ decl_storage! {
 // https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, AccountId),
+		Accumulated(u128, AccountId),
 	}
 );
 
 // Errors inform users that something went wrong.
 decl_error! {
 	pub enum Error for Module<T: Trait> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		AccumulationOverflow,
 	}
 }
 
@@ -63,41 +62,30 @@ decl_module! {
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
 
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
+		#[weight = 100]
+		pub fn set_increment(origin, increment: u128) -> dispatch::DispatchResult {
+			let _ = ensure_signed(origin)?;
 
-			// Update storage.
-			Something::put(something);
+			let mut acc = Self::acc();
+			acc.increment_per_call = increment;
 
-			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			// Return a successful DispatchResult
+			Acc::put(acc);
+
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
+		#[weight = 100]
+		pub fn accumulate(origin) -> dispatch::DispatchResult {
+			let who = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
+			let mut acc = Self::acc();
+			acc.current_count = acc.current_count.checked_add(acc.increment_per_call).ok_or(Error::<T>::AccumulationOverflow)?;
+
+			Acc::put(acc.clone());
+
+			Self::deposit_event(RawEvent::Accumulated(acc.current_count, who));
+
+			Ok(())
 		}
 	}
 }
